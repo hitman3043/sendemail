91a7787ec5d5461615b69b24636b6ec2e61ec241
Yogesh Raghav <yogeshraghav3043@gmail.com>
2020-12-12 02:32:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include 'src/VerifyEmail.php';
+$ve = new hbattat\VerifyEmail('yahoo.com', 'hitman_codename408@yahoo.com', 25);
+$ve->set_socket_timeout(3);
+$ve->set_proxy('208.80.28.208', '8080');
+var_dump($ve->verify());
+echo "<pre>";
+print_r($ve->get_debug());
+echo "</pre>";
